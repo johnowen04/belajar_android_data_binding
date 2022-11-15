50221b2f07205c49f700932e1363fa36ea57c007
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-15 14:27:14 +0700

    Initiating Project

---
+++ app/src/main/java/com/example/belajarandroiddatabinding/view/EditTodoFragment.kt
@@ -0,0 +1,66 @@
+package com.example.belajarandroiddatabinding.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.belajarandroiddatabinding.R
+import com.example.belajarandroiddatabinding.viewmodel.DetailTodoViewModel
+import kotlinx.android.synthetic.main.fragment_create_todo.*
+
+class EditTodoFragment : Fragment() {
+    private lateinit var viewModel: DetailTodoViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textTitleTodo.text = "Edit Todo"
+        btnAddNotes.text = "Save Changes"
+
+        val uuid: Int = EditTodoFragmentArgs.fromBundle(requireArguments()).uuid
+
+        viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
+        viewModel.fetch(uuid)
+
+        btnAddNotes.setOnClickListener {
+            val radio = view.findViewById<RadioButton>(radioGroupPriority.checkedRadioButtonId)
+            viewModel.updateTodo(
+                editTitle.text.toString(),
+                editNotes.text.toString(),
+                radio.tag.toString().toInt(),
+                uuid
+            )
+
+            Toast.makeText(view.context, "Data updated", Toast.LENGTH_SHORT).show()
+            Navigation.findNavController(it).popBackStack()
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.todoLiveData.observe(viewLifecycleOwner) {
+            editTitle.setText(it.title)
+            editNotes.setText(it.notes)
+
+            when (it.priority) {
+                3 -> radioHigh.isChecked = true
+                2 -> radioMedium.isChecked = true
+                else -> radioLow.isChecked = true
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/belajarandroiddatabinding/view/TodoListAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.belajarandroiddatabinding.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.belajarandroiddatabinding.R
+import com.example.belajarandroiddatabinding.model.Todo
+import kotlinx.android.synthetic.main.todo_item_layout.view.*
+
+class TodoListAdapter(
+    val todoList: ArrayList<Todo>,
+    val adapterOnClick: (Todo) -> Unit
+): RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(var view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.todo_item_layout, parent, false)
+
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val todo = todoList[position]
+
+        with(holder.view) {
+            checkBox.text = todo.title
+            checkBox.setOnCheckedChangeListener{ _, b ->
+                if (b) adapterOnClick(todo)
+            }
+
+            imgButtonEdit.setOnClickListener {
+                val action = TodoListFragmentDirections.actionEditTodoFragment(todo.uuid)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+    }
+
+    fun updateTodoList(newTodoList: List<Todo>) {
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = todoList.size
+}
