9868fdc762caafe4043b5bee7e09489df05bb8ec
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-15 15:07:39 +0700

    Modify TodoListAdapter to use DataBinding. Implementing onClick Listener using Interfaces.

--- app/src/main/java/com/example/belajarandroiddatabinding/view/Interfaces.kt
+++ app/src/main/java/com/example/belajarandroiddatabinding/view/Interfaces.kt
@@ -1,8 +1,13 @@
 package com.example.belajarandroiddatabinding.view
 
+import android.view.View
 import android.widget.CompoundButton
 import com.example.belajarandroiddatabinding.model.Todo
 
 interface TodoCheckedChangeListener {
     fun onCheckedChange(cb: CompoundButton, isChecked: Boolean, todo: Todo)
+}
+
+interface TodoEditClickListener {
+    fun onClick(view: View)
 }
--- app/src/main/java/com/example/belajarandroiddatabinding/view/TodoListAdapter.kt
+++ app/src/main/java/com/example/belajarandroiddatabinding/view/TodoListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.belajarandroiddatabinding.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.navigation.Navigation
@@ -11,8 +12,9 @@ import com.example.belajarandroiddatabinding.model.Todo
 class TodoListAdapter(
     val todoList: ArrayList<Todo>,
     val adapterOnClick: (Todo) -> Unit
-): RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>(),
-TodoCheckedChangeListener {
+):  RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>(),
+    TodoCheckedChangeListener,
+    TodoEditClickListener {
 
     class TodoViewHolder(var view: TodoItemLayoutBinding): RecyclerView.ViewHolder(view.root)
 
@@ -26,6 +28,7 @@ TodoCheckedChangeListener {
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.view.todo = todoList[position]
         holder.view.listener = this
+        holder.view.editListener = this
 
         /*
         holder.view.listener = object: TodoCheckedChangeListener {
@@ -34,6 +37,14 @@ TodoCheckedChangeListener {
             }
         }
          */
+
+        /*
+        holder.view.editListener = object: TodoEditClickListener {
+            override fun onClick(view: View) {
+                TODO("Not yet implemented")
+            }
+        }
+        */
 
         /*
         val selectedTodo = todoList[position]
@@ -62,5 +73,10 @@ TodoCheckedChangeListener {
 
     override fun onCheckedChange(cb: CompoundButton, isChecked: Boolean, todo: Todo) {
         if (isChecked) adapterOnClick(todo)
+    }
+
+    override fun onClick(view: View) {
+        val action = TodoListFragmentDirections.actionEditTodoFragment(view.tag.toString().toInt())
+        Navigation.findNavController(view).navigate(action)
     }
 }
